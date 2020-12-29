106bf98112c4365cd90a86128cd9207c22fb8461
RukInDaHouse <r1997p@mail.ru>
2020-12-29 17:50:15 +0300

    Add timeout script

---
+++ result/js/main.js
@@ -0,0 +1,6 @@
+var container = document.getElementsByClassName('container');
+    setTimeout(function(){
+        container[0].classList.add('invisible');
+        container[1].classList.remove('invisible');
+    }, 16000);
+
