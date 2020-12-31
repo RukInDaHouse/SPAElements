6b31b043b0d5c3a993af33e6ea3781d78515b77f
RukInDaHouse <r1997p@mail.ru>
2020-12-31 15:50:00 +0300

    Add new time scipts to card scene

--- result/js/main.js
+++ result/js/main.js
@@ -1,12 +1,52 @@
+var map = document.getElementsByClassName('map');
+var helloscreen = document.getElementsByClassName('helloscreen');
 var container = document.getElementsByClassName('container');
-var node = document.getElementById("nested");
+var audio = document.getElementsByTagName('audio');
+var arrow = document.getElementsByClassName('arrownext');
+
+
+map[2].onclick = function() {
+    var audio = new Audio(); 
+    audio.src = 'volume/ep1.mp3'; 
+    audio.autoplay = true; 
+    
+    helloscreen[0].classList.add('invisible');
 
     setTimeout(function(){
-        if (node.parentNode) {
-  node.parentNode.removeChild(node);
-}
+    	map[4].classList.remove('invisible');
+    	map[4].classList.add('fullscreen__leaf');
+    }, 33000);
+
+    setTimeout(function(){
+    	var findElement = container[0];
+        findElement.parentNode.removeChild(findElement);
+    }, 40000);
+
+    setTimeout(function(){
         container[0].classList.remove('invisible');
-    }, 2000);
+    }, 37000);
 
+    setTimeout(function(){
+        var audio = new Audio(); 
+        audio.src = 'volume/ep2.mp3'; 
+        audio.autoplay = true;
+    }, 37000);
 
+    setTimeout(function(){
+        container[2].classList.remove('invisible');
+    }, 38000);
 
+    setTimeout(function(){
+        container[3].classList.remove('invisible');
+    }, 44000);
+}
+
+map[0].onclick = function() {  
+    helloscreen[0].classList.add('flashmodal');
+}
+
+arrow[0].onclick = function() {  
+    var findElement = container[0];
+    findElement.parentNode.removeChild(findElement);
+    container[0].classList.remove('invisible');
+}
