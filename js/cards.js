e64f9db125178521ace07a8eb83d62c19534d671
RukInDaHouse <r1997p@mail.ru>
2021-01-08 19:18:13 +0300

    New folder

--- js/cards.js
+++ js/cards.js
@@ -5,6 +5,9 @@ var allCards = document.querySelectorAll('.flip-card');
 var nope = document.getElementById('nope');
 var love = document.getElementById('love');
 var next = document.getElementById('next');
+var quizcardbuttons = document.querySelectorAll('quizcard--buttons');
+var arrow = document.getElementsByClassName('arrownext');
+
 
 function initCards(card, index) {
   var newCards = document.querySelectorAll('.flip-card:not(.removed)');
@@ -24,7 +27,7 @@ function createButtonListener(love) {
     var cards = document.querySelectorAll('.flip-card:not(.removed)');
     var next = document.querySelectorAll('.next:not(.removed)');
 
-    if (!cards.length) return false;
+    if (!cards.length) document.getElementsByClassName('arrownext')[0].classList.remove('invisible');
 
     var card = cards[0];
     var nextbutton = next[0];
@@ -39,13 +42,19 @@ function createButtonListener(love) {
     } else {
       card.classList.toggle('do-flip');
       card.style.zIndex = 1000000000000000000;
-            nextbutton.onclick = function() {
-      card.style.transform = 'translate(-100vw, -100px) rotate(30deg) rotateY(-180deg)';
-      cards[1].classList.remove('hidden');
+        document.getElementById('nope').style.opacity = '0';
+        document.getElementById('love').style.opacity = '0';
+
+      nextbutton.onclick = function() {
+        card.style.zIndex = 1000000000000000000;
+        card.style.transform = 'translate(-100vw, -100px) rotate(30deg) rotateY(-180deg)';
+        cards[1].classList.remove('hidden');
+        document.getElementById('nope').style.opacity = '1';
+        document.getElementById('love').style.opacity = '1';
   };
 
     }
-
+    
     initCards();
     event.preventDefault();
   };
---
+++ js/main.js
@@ -0,0 +1,112 @@
+var map = document.getElementsByClassName('map');
+var helloscreen = document.getElementsByClassName('helloscreen');
+var container = document.getElementsByClassName('container');
+var audio = document.getElementsByTagName('audio');
+var arrow = document.getElementsByClassName('arrownext');
+var header = document.getElementsByClassName('headerdisable');
+var mapbackgroundimage = document.getElementsByClassName('mapbackgroundimage');
+
+
+container[0].onclick = function() {
+    document.getElementsByClassName('popup-wrapper')[0].classList.add('actived');
+
+}
+
+
+
+map[2].onclick = function() {
+    var audio = new Audio(); 
+    audio.src = 'volume/ep1.mp3'; 
+    audio.autoplay = true; 
+    header[0].classList.add('invisible');
+    header[1].classList.add('invisible');
+    header[2].classList.add('invisible');
+    helloscreen[0].classList.add('invisible');
+    map[5].classList.remove('invisible');
+    map[5].classList.add('step__zero');
+    mapbackgroundimage[1].classList.add('zoom__small__parents');
+    mapbackgroundimage[2].classList.add('zoom__small__ballun');
+    mapbackgroundimage[3].classList.add('zoom__small__balluncenter');
+    mapbackgroundimage[4].classList.add('zoom__small__cross');
+    mapbackgroundimage[5].classList.add('zoom__small__pump');
+    mapbackgroundimage[6].classList.add('zoom__small__sign');
+    mapbackgroundimage[7].classList.add('zoom__small__stage');
+
+
+    setTimeout(function(){
+    	map[4].classList.remove('invisible');
+    	map[4].classList.add('fullscreen__leaf');
+    }, 27000);
+
+    setTimeout(function(){
+    	var findElement = container[0];
+        findElement.parentNode.removeChild(findElement);
+    }, 36000);
+
+    setTimeout(function(){
+        container[0].classList.remove('invisible');
+        container[0].classList.add('falling-leaves');
+    }, 36000);
+
+    setTimeout(function(){
+        var audio = new Audio(); 
+        audio.src = 'volume/ep2.mp3'; 
+        audio.autoplay = true;
+    }, 40000);
+
+}
+
+map[0].onclick = function() {  
+    helloscreen[0].classList.add('flashmodal');
+}
+
+arrow[0].onclick = function() {  
+    var findElement = container[0];
+    findElement.parentNode.removeChild(findElement);
+    container[0].classList.remove('invisible');
+
+    setTimeout(function(){
+        map[2].classList.remove('invisible');
+        map[2].classList.add('fullscreen__leaf');
+    }, 6000);
+
+    setTimeout(function(){
+        var findElement = container[0];
+        findElement.parentNode.removeChild(findElement);
+        container[0].classList.remove('invisible');
+    }, 9000);
+}
+
+arrow[0].onclick = function() {  
+ document.getElementsByClassName('arrownext')[0].disabled = true;
+    var audio = new Audio(); 
+        audio.src = 'volume/ep3.mp3'; 
+        audio.autoplay = true;
+
+    setTimeout(function(){
+        map[0].classList.remove('invisible');
+        map[0].classList.add('fullscreen__leaf');
+    }, 12000);
+
+    setTimeout(function(){
+        var findElement = container[0];
+        findElement.parentNode.removeChild(findElement);
+        container[0].classList.remove('invisible');
+        var audio = new Audio(); 
+        audio.src = 'volume/ep4.mp3'; 
+        audio.autoplay = true;
+    }, 16000);
+
+    setTimeout(function(){
+        map[2].classList.remove('invisible');
+        map[2].classList.add('fullscreen__leaf');
+    }, 30000);
+
+    setTimeout(function(){
+        var findElement = container[0];
+        findElement.parentNode.removeChild(findElement);
+        container[0].classList.remove('invisible');
+    }, 21000);
+}
+
+
